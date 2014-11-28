4cec6a3a9a490e89019f41a78ea956b67128fc6e
Bartosz Kosarzycki <bartek@brodline.com>
2014-11-28 01:41:39 +0100

    upload file method

--- app/src/main/java/com/bizbeam/s3/uploader/S3Connector.java
+++ app/src/main/java/com/bizbeam/s3/uploader/S3Connector.java
@@ -4,14 +4,18 @@ import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import org.apache.log4j.Logger;
+
+import java.io.File;
 
 public class S3Connector {
 
     public String mAWS_S3_BUCKET;
     public String mAWS_ACCESS_KEY;
     public String mAWS_SECRET_KEY;
-    public AmazonS3 amazonS3;
+    public AmazonS3 mAmazonS3;
 
     public S3Connector(String s3Bucket, String s3AccessKey, String s3SecretKey) {
         mAWS_S3_BUCKET = s3Bucket;
@@ -22,11 +26,19 @@ public class S3Connector {
     public void initialize() {
         if ((mAWS_ACCESS_KEY != null) && (mAWS_SECRET_KEY != null)) {
             AWSCredentials awsCredentials = new BasicAWSCredentials(mAWS_ACCESS_KEY, mAWS_SECRET_KEY);
-            amazonS3 = new AmazonS3Client(awsCredentials);
-            //amazonS3.createBucket(s3Bucket);
+            mAmazonS3 = new AmazonS3Client(awsCredentials);
+            //mAmazonS3.createBucket(s3Bucket);
 
             LOGGER.info("Using S3 Bucket: " + mAWS_S3_BUCKET);
         }
+    }
+
+    public void uploadFile(File file) {
+
+        PutObjectRequest putObjectRequest = new PutObjectRequest(mAWS_S3_BUCKET, file.getName(), file);
+        putObjectRequest.withCannedAcl(CannedAccessControlList.PublicRead); // public for all
+        mAmazonS3.putObject(putObjectRequest); // upload file
+        
     }
 
     private static final Logger LOGGER = Logger.getLogger(S3Connector.class);
