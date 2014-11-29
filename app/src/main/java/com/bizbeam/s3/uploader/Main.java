58150aa3c0064c5e43fd85b27c36e88d703ff042
Bartosz Kosarzycki <bartek@brodline.com>
2014-11-29 02:14:17 +0100

    send sample files

--- app/src/main/java/com/bizbeam/s3/uploader/Main.java
+++ app/src/main/java/com/bizbeam/s3/uploader/Main.java
@@ -1,7 +1,31 @@
 package com.bizbeam.s3.uploader;
 
+import org.apache.log4j.Logger;
+
+import java.io.File;
+
 public class Main {
+
+    private static String AWSAccessKeyId= "SAMPLESAMPLESAMPLE";
+    private static String AWSSecretKey="2SamPleSample3456SaMpleSample3578SampleSaMpLe";
+    private static String AWSBucketName = "bizbeamtest";
+    private static String FILE_PREFIX_PATH = "app/src/main/resources";
+
 	public static void main (String args[]) {
-		System.out.println ("Hello World!");
-	}
+
+        try {
+            System.out.println ("Initializing connector...");
+            S3Connector s3Connector = new S3Connector(AWSBucketName, AWSAccessKeyId, AWSSecretKey);
+            s3Connector.initialize();
+
+            //put two files
+            s3Connector.uploadFile(new File(FILE_PREFIX_PATH, "AdobeXMLFormsSamples.pdf"));
+            s3Connector.uploadFile(new File(FILE_PREFIX_PATH, "nature-wallpaper-27.jpg"));
+
+        } catch (Exception exc) {
+            LOGGER.error(exc);
+        }
+    }
+
+    private static final Logger LOGGER = Logger.getLogger(Main.class);
 }
